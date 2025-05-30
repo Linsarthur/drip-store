022efe7d68f824ecfc6208b37388a51bd069a826
Arthur Cavalcante <linsarthur02@gmail.com>
2025-05-30 09:13:41 -0300

    fix

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const AXIOS = axios.create({
-    baseURL: "https://drip-store-umber.vercel.app"
+    baseURL: "https://drip-store-a3bdhvou4-linsarthurs-projects.vercel.app"
 })
